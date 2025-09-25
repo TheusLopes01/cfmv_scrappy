ce47607dcbd70ea4d568517b02d2ea1a4a659b9b
TheusLopes01 <mslopesilva01@gmail.com>
2025-09-25 19:40:22 -0300

    1.2

--- cfmv_scrap.py
+++ cfmv_scrap.py
@@ -6,6 +6,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import TimeoutException
+from vet_list_sp import formatList
 import time
 
 
@@ -19,23 +21,43 @@ servico = Service(ChromeDriverManager().install())
 navegador = webdriver.Chrome(service=servico, options=chrome_options)
 navegador.get('https://app.cfmv.gov.br/paginas/busca')
 
-formUf = navegador.find_element(By.NAME, 'filtro_uf')
+allRecords = []
 
-dropdownSelect = Select(formUf)
-dropdownSelect.select_by_value("SP")
+for vet in formatList:
+    # Seleciona UF
+    formUf = navegador.find_element(By.NAME, 'filtro_uf')
+    Select(formUf).select_by_value("SP")
 
-inputTpText = navegador.find_element(By.CSS_SELECTOR, "input[name= 'filtro_tp_texto'][value= '2']")
-inputTpText.click()
+    # Seleciona tipo de busca
+    navegador.find_element(By.CSS_SELECTOR, "input[name='filtro_tp_texto'][value='2']").click()
+    navegador.find_element(By.CSS_SELECTOR, "input[name='filtro_procurar'][value='2']").click()
 
-inputSearch = navegador.find_element(By.CSS_SELECTOR, "input[name= 'filtro_procurar'][value= '2']")
-inputSearch.click()
+    # Preenche input
+    inputForm = navegador.find_element(By.NAME, 'filtro_texto')
+    #inputForm.clear()
+    inputForm.send_keys(vet)
 
-inputForm = navegador.find_element(By.NAME, 'filtro_texto').send_keys("SP0039953")
+    # Clica em filtrar
+    SearchButton = navegador.find_element(By.CSS_SELECTOR, "input.btn-info.btn-padrao.col-5[value='Filtrar']")
+    SearchButton.click()
 
-SearchButton = navegador.find_element(By.CSS_SELECTOR, "input[class= 'btn-info btn-padrao col-5'][value= 'Filtrar']")
-SearchButton.click()
+    # Checa se apareceu mensagem de erro
+    try:
+        errorButton = WebDriverWait(navegador, 3).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "input.ui-button.ui-corner-all.ui-widget"))
+        )
+        errorButton.click()
+        print(f"Erro ao buscar {vet}, cliquei no botão de erro.")
+        continue  # pula para o próximo vet
+    except TimeoutException:
+        pass
 
-time.sleep(2)
-result = navegador.find_element(By.ID, "relatorio")
+    # Pega resultados
+    time.sleep(0.5)
+    result = navegador.find_element(By.ID, "relatorio")
+    print(result.text)
 
-print(result.text)
+    # Prepara novo search
+    navegador.find_element(By.CSS_SELECTOR, "h2.texto-verde.text-center.mb-2.mt-2.border-bottom.filtrosDatatable.col-12.border").click()
+    time.sleep(0.5)
+
---
+++ vet_list_sp.py
@@ -0,0 +1,8 @@
+vetList ="""
+SP0060822
+SP0062685
+SP0063940
+SP0062041
+SP0000403"""
+
+formatList = [vet.strip() for vet in vetList.split("\n") if vet.strip()]
